acc2c584a0f0149e384e7522a0f26ea77879f875
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-12 18:55:23 +0800

    注解

--- src/main/java/com/pig/design/listener/MailEventListener.java
+++ src/main/java/com/pig/design/listener/MailEventListener.java
@@ -1,15 +1,18 @@
 package com.pig.design.listener;
 
+import com.pig.design.config.ThreadPoolConfig;
 import com.pig.design.dto.MailDTO;
 import com.pig.design.event.BaseEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class MailEventListener {
 
+    @Async(ThreadPoolConfig.EVENT_THREAD_POOL)
     @EventListener
     public void listen(BaseEvent<MailDTO> event){
         MailDTO mailDTO = event.getData();
