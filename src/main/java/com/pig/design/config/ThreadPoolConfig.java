e1cb498e4c19cd07c861c9b1da5acdd3cb774ee2
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-12 18:53:23 +0800

    添加线程池

---
+++ src/main/java/com/pig/design/config/ThreadPoolConfig.java
@@ -0,0 +1,30 @@
+package com.pig.design.config;
+
+import org.apache.tomcat.util.threads.ThreadPoolExecutor;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolConfig {
+
+    // private static final int CPU_NUM = Runtime.getRuntime().availableProcessors();
+    // io密集型任务定义
+    // private static final int MAX_POOL_SIZE = CPU_NUM * 2;
+    // cpu密集型任务定义
+    // private static final int MAX_POOL_SIZE = CPU_NUM + 1;
+
+
+    public static final String EVENT_THREAD_POOL = "EVENT_THREAD_POOL";
+
+    @Bean(EVENT_THREAD_POOL)
+    public ThreadPoolExecutor eventTHreadPool(){
+        return  new ThreadPoolExecutor(
+                3,
+                5,
+                30,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(1024)
+        );
+    }
+}
