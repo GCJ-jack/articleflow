dff72585d26ba6886a4368299636c817c4622703
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-12 17:59:44 +0800

    push dto

---
+++ src/main/java/compig/design/dto/MailDTO.java
@@ -0,0 +1,16 @@
+package compig.design.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MailDTO {
+
+    private String mail;
+
+    private String title;
+
+    private String content;
+
+}
