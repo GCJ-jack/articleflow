301dcb0380fb7d7df74a21f45951d0a3cdae1673
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-13 16:16:40 +0800

    邮件模式freemaker

--- src/main/java/com/pig/design/controller/EventController.java
+++ src/main/java/com/pig/design/controller/EventController.java
@@ -1,15 +1,27 @@
 package com.pig.design.controller;
 
+import cn.hutool.core.collection.ListUtil;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.extra.spring.SpringUtil;
 import com.pig.design.dto.MailDTO;
 import com.pig.design.event.BaseEvent;
+import freemarker.template.Template;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jms.JmsProperties;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import javax.annotation.Resource;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -18,6 +30,9 @@ public class EventController {
 
     @Resource
     private ApplicationEventPublisher publisher;
+
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
 
     @GetMapping("/file-export")
     public String fileExport(){
@@ -33,5 +48,29 @@ public class EventController {
         SpringUtil.publishEvent(new BaseEvent<>(mailDTO, this));
 
         return "ok";
+    }
+
+    @SneakyThrows
+    @GetMapping("/test")
+    private String testFtl(String username){
+        Template template = freeMarkerConfigurer.getConfiguration().getTemplate("test.txt.ftl");
+
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("username",username);
+        map.put("curDate", DateUtil.format(LocalDateTime.now(),"yyyy-MM-dd HH:mm:ss"));
+
+        map.put("hobbies", ListUtil.of("吃饭","睡觉"));
+
+        //输出为字符串
+        StringWriter resultStr = new StringWriter();
+
+        //渲染模版
+        template.process(map, resultStr);
+
+        //关闭流
+        resultStr.close();
+
+        return  resultStr.toString();
     }
 }
--- src/main/java/com/pig/design/listener/MailEventListener.java
+++ src/main/java/com/pig/design/listener/MailEventListener.java
@@ -3,20 +3,61 @@ package com.pig.design.listener;
 import com.pig.design.config.ThreadPoolConfig;
 import com.pig.design.dto.MailDTO;
 import com.pig.design.event.BaseEvent;
+import freemarker.template.Template;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.StringWriter;
 
 @Slf4j
 @Component
 public class MailEventListener {
 
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+
     @Async(ThreadPoolConfig.EVENT_THREAD_POOL)
+    @Retryable(value = Exception.class, maxAttempts = 3, backoff = @Backoff(delay = 1000L))
     @EventListener
     public void listen(BaseEvent<MailDTO> event){
         MailDTO mailDTO = event.getData();
 
         log.info(mailDTO.getMail() + " " + mailDTO.getContent());
+
+        String htmlContent = buildEmailHtml(mailDTO);
+
+        log.info(htmlContent);
     }
+
+    @Recover
+    public void recover(Exception e, BaseEvent<MailDTO> event){
+        // 记录日志
+        log.error("邮件发送失败", e);
+    }
+
+    @SneakyThrows
+    public String buildEmailHtml(MailDTO mailDTO){
+        Template template = freeMarkerConfigurer.getConfiguration().getTemplate("email.html.ftl");
+
+        //输出为字符串
+        StringWriter stringWriter = new StringWriter();
+
+        //渲染模版
+        template.process(mailDTO,stringWriter);
+
+        stringWriter.close();
+
+        return  stringWriter.toString();
+    }
+
+
+
 }
